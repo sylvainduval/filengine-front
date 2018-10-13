fc7505c1d934d1b09d9733a9401c4d5a5df8685f
Sylvain DUVAL <sylv1duval@gmail.com>
2018-10-13 10:57:03 +0200

    Adding Chips form libraries in user Form

---
+++ src/components/AutocompleteSelectLibrary.js
@@ -0,0 +1,244 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import deburr from 'lodash/deburr';
+import keycode from 'keycode';
+import Downshift from 'downshift';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+//import Popper from '@material-ui/core/Popper';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import Chip from '@material-ui/core/Chip';
+
+import api from "../utils/api.js";
+
+let suggestions = [];
+
+function renderInput(inputProps) {
+	const { InputProps, classes, ref, ...other } = inputProps;
+
+	return (
+		<TextField
+			InputProps={{
+			inputRef: ref,
+			classes: {
+			root: classes.inputRoot,
+			},
+			...InputProps,
+			}}
+			{...other}
+		/>
+	);
+}
+
+function renderSuggestion({ suggestion, index, itemProps, highlightedIndex, selectedItem }) {
+	const isHighlighted = highlightedIndex === index;
+	const isSelected = (selectedItem || '').indexOf(suggestion.id) > -1;
+	
+	return (
+		<MenuItem
+			{...itemProps}
+			key={suggestion._id}
+			selected={isHighlighted}
+			component="div"
+			style={{
+				fontWeight: isSelected ? 500 : 400,
+			}}
+		>
+			{suggestion.id}
+		</MenuItem>
+	);
+}
+
+renderSuggestion.propTypes = {
+	highlightedIndex: PropTypes.number,
+	index: PropTypes.number,
+	itemProps: PropTypes.object,
+	selectedItem: PropTypes.string,
+	suggestion: PropTypes.shape({ id: PropTypes.string }).isRequired,
+};
+
+function getSuggestions(value) {
+	const inputValue = deburr(value.trim()).toLowerCase();
+	const inputLength = inputValue.length;
+	let count = 0;
+
+	return inputLength === 0
+	? []
+	: suggestions.filter(suggestion => {
+		const keep =
+			count < 4 && suggestion.id.slice(0, inputLength).toLowerCase() === inputValue;
+		
+		if (keep) {
+			count += 1;
+		}
+		
+		return keep;
+	});
+}
+
+class AutocompleteSelectLibrary extends React.Component {
+	state = {
+		inputValue: '',
+		selectedItem: []
+	};
+
+	handleKeyDown = event => {
+		const { inputValue, selectedItem } = this.state;
+		if (selectedItem.length && !inputValue.length && keycode(event) === 'backspace') {
+			this.setState({
+				selectedItem: selectedItem.slice(0, selectedItem.length - 1),
+			});
+		}
+	};
+
+	handleInputChange = event => {
+		this.setState({ inputValue: event.target.value });
+	};
+
+	handleChange = item => {
+		let { selectedItem } = this.state;
+		
+		if (selectedItem.indexOf(item) === -1) {
+			selectedItem = [...selectedItem, item];
+		}
+		
+		this.setState({
+			inputValue: '',
+			selectedItem,
+		});
+		
+		
+	};
+
+	handleDelete = item => () => {
+		this.setState(state => {
+			const selectedItem = [...state.selectedItem];
+			selectedItem.splice(selectedItem.indexOf(item), 1);
+			return { selectedItem };
+		});
+	};
+	
+	componentDidMount() {		
+		api.request(
+			{
+				method: 'get',
+				url: '/libraries',
+				data: {}
+			}, function(data, status) { //success
+				suggestions = data.data;
+			});
+	}
+
+	componentWillReceiveProps(nextProps) {
+		let { selectedItem } = this.state;
+		for (const item of nextProps.list) {
+			if (selectedItem.indexOf(item.id) === -1) {
+				selectedItem = [...selectedItem, item.id];
+			}
+		}
+		this.setState({
+			selectedItem,
+		});
+	}
+		
+	componentDidUpdate() {
+		this.props.onChange(this.state.selectedItem);
+	}
+
+	render() {
+		const { classes } = this.props;
+		const { inputValue, selectedItem } = this.state;
+
+		return (
+			<div className={classes.root}>
+				<Downshift
+					id="downshift-multiple"
+					inputValue={inputValue}
+					onChange={this.handleChange}
+					selectedItem={selectedItem}
+				>
+					{({
+						getInputProps,
+						getItemProps,
+						isOpen,
+						inputValue: inputValue2,
+						selectedItem: selectedItem2,
+						highlightedIndex,
+					}) => (
+						<div className={classes.container}>
+							{renderInput({
+								fullWidth: true,
+								classes,
+								InputProps: getInputProps({
+									startAdornment: selectedItem.map(item => (
+										<Chip
+											key={item}
+											tabIndex={-1}
+											label={item}
+											className={classes.chip}
+											onDelete={this.handleDelete(item)}
+										/>
+										)),
+									onChange: this.handleInputChange,
+									onKeyDown: this.handleKeyDown,
+									placeholder: selectedItem.length === 0 ? 'Select multiple libraries' : '',
+								}),
+								label: 'Libraries',
+							})}
+							{isOpen ? (
+								<Paper className={classes.paper} square>
+									{getSuggestions(inputValue2).map((suggestion, index) =>
+										renderSuggestion({
+											suggestion,
+											index,
+											itemProps: getItemProps({ item: suggestion.id }),
+											highlightedIndex,
+											selectedItem: selectedItem2,
+										}),
+									)}
+							  </Paper>
+							) : null}
+						</div>
+					)}
+				</Downshift>
+			</div>
+		);
+	}
+}
+
+AutocompleteSelectLibrary.propTypes = {
+	classes: PropTypes.object.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    /*height: 150*/
+  },
+  container: {
+    flexGrow: 1,
+    position: 'relative',
+  },
+ paper: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0,
+    
+  },
+  chip: {
+    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`,
+  },
+  inputRoot: {
+    flexWrap: 'wrap',
+  }
+});
+
+
+AutocompleteSelectLibrary.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AutocompleteSelectLibrary);
--- src/views/AdminUser.js
+++ src/views/AdminUser.js
@@ -6,104 +6,170 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
+//import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Grid from '@material-ui/core/Grid';
+
+import AutocompleteSelectLibrary from '../components/AutocompleteSelectLibrary';
+import api from "../utils/api.js";
 
 const styles = theme => ({
 
 	formControl: {
 		margin: theme.spacing.unit / 2,
+		width: '100%'
 	},
   button: {
     margin: theme.spacing.unit,
   },
 });
 
+//Listes des librairies de l'utilisateur telles qu'affichées dans le formulaire
+let formLibraries = []
 
 class AdminUser extends React.Component {
 	
 	constructor(props) {
-	    super(props);
+		super(props);
 
-		 this.state = {
+		this.state = {
 			open: false,
+			//Si register vaut true, il faudra inscrire l'utilisateur (POST /register)
+			//puis écrire ses données (PUT /user/_id)
+			register: false,
+			data: {},
+			msg: {},
 		};
+		
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
   	}
   	
 	handleClose = () => {
-	    	this.setState({ open: false });
-	    	this.props.onClose();
-	  };
+		this.setState({ open: false, data: {}, msg: {}, register: false });
+		this.props.onClose();
+	};
+	
+	updateList = data => {
+	    this.setState({
+	      data: data
+	    });
+	}
 
 	componentWillReceiveProps(nextProps) {
-	  // Typical usage (don't forget to compare props):
-	  if (this.props.open !== nextProps.open) {
+		// Typical usage (don't forget to compare props):
+		if (this.props.open !== nextProps.open) {
 			this.setState({
-				open: nextProps.open
+				open: nextProps.open,
+				register: nextProps.userID === null ? true : false
 			})
-	  }
+		}
+
+		if (nextProps.userID !== null && nextProps.open === true) {
+			
+			const updateList = this.updateList;
+			
+			api.request(
+				{
+					method: 'get',
+					url: '/user/'+nextProps.userID
+				}, function(data, status) { //success
+					updateList(data.data);
+				}
+			);
+		}
+	}
+	
+	handleChange(event) {
+
+		const target = event.target;
+		const { data, msg } = this.state;
+		
+	    let value = target.type === 'checkbox' ? target.checked : target.value;
+	    //value = target.type === 'text' || target.tagName === 'TEXTAREA' ? value.toUpperCase() : value;
+	    data[target.id] = value;
+	    
+	    msg[target.id] = null;
+	    msg[target.id] = target.type === 'email' && value.indexOf('@') === -1 ? "Wrong email" : msg[target.id];
+	    msg[target.id] = target.type === 'text' && target.required && !value.length ? "Required" : msg[target.id];
+
+	    // update state
+	    this.setState({
+	        data,
+	        msg
+	    });
+	}
+	
+	handleSubmit(event) {
+		
+		console.log('submit !');
+		console.log(this.state);
+		console.log(formLibraries);
+		this.handleClose();
+		
+		event.preventDefault();
 	}
 	
 	render() {
 		const { classes, userID } = this.props;
+		const { data, msg } = this.state;
+		
+		//console.log(data);
 
 		return (
+			
 			<Dialog
 				open={this.state.open}
-	          scroll="paper"
-	          aria-labelledby="Edit User"
-	          onClose={this.handleClose}
-	        >
-	          <DialogTitle id="scroll-dialog-title">{userID ? userID : "Add user"}</DialogTitle>
-	          <DialogContent>
-	            <DialogContentText>
-	              Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac
-	              facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum
-	              at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus
-	              sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum
-	              nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur
-	              et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras
-	              mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-	              egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-	              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-	              lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-	              sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-	              Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
-	              consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-	              egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-	              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-	              lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-	              sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-	              Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
-	              consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-	              egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-	              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-	              lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-	              sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-	              Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
-	              consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-	              egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-	              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-	              lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-	              sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-	              Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis
-	              consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in,
-	              egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-	              Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-	              lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean lacinia bibendum nulla
-	              sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-	              Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
-	            </DialogContentText>
-	          </DialogContent>
-	          <DialogActions>
-	            <Button onClick={this.handleClose} color="primary">
-	              Cancel
-	            </Button>
-	            <Button onClick={this.handleClose} color="primary">
-	              Save
-	            </Button>
-	          </DialogActions>
-	        </Dialog>
+				scroll="paper"
+				aria-labelledby="Edit User"
+				onClose={this.handleClose}
+			>
+				<DialogTitle id="scroll-dialog-title">{userID ? "Edit user" : "Add user"}</DialogTitle>
+
+				<form onSubmit={this.handleSubmit}>
+					<DialogContent>
+						<Grid container direction="column" alignItems="center">
+							<Grid container item xs={12}>
+								<FormControl fullWidth className={classes.formControl} error={!!msg.login}>
+									<InputLabel htmlFor="login">Login</InputLabel>
+									<Input id="login" value={data.login ? data.login : ''} onChange={this.handleChange} />
+									<FormHelperText>{msg.login}</FormHelperText>
+								</FormControl>
+								
+								<FormControl fullWidth className={classes.formControl}  error={!!msg.password}>
+									<InputLabel htmlFor="password">Password</InputLabel>
+									<Input type="password" id="password" />
+									<FormHelperText>{msg.password}</FormHelperText>
+								</FormControl>
+								
+								<div className={classes.formControl + ' mb-3'}>
+									<AutocompleteSelectLibrary 
+										list={data.libraries ? data.libraries : []}
+										onChange={(libraries) => formLibraries = libraries} 
+									/>
+								</div>
+	
+								<FormControl fullWidth className={classes.formControl} error={!!msg.email}>
+									<InputLabel htmlFor="email">Email</InputLabel>
+									<Input type="email" id="email" value={data.email ? data.email : ''} onChange={this.handleChange} />
+									<FormHelperText>{msg.email}</FormHelperText>
+								</FormControl>
+								
+							</Grid>
+						</Grid>
+
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={this.handleClose} color="primary">Cancel</Button>
+						<Button type="submit" color="primary">Save</Button>
+					</DialogActions>
+				</form>
+			</Dialog>
 		);
 	}
 }
